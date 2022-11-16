4c7f8523d4c59e464641ce3f2eb039595f13f9f3
DerylIvan <118355784+DerylIvan@users.noreply.github.com>
2022-11-16 18:37:15 +0700

    view

---
+++ view/templates/sidebar.php
@@ -0,0 +1,54 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4 bg-dark">
+    <!-- Brand Logo -->
+    <a href="admin.php" class="brand-link">
+        <img src="../../assets/img/tool.png" alt="Toko Gembul Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Toko Gembul Motor</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar-dark link">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2-dark link">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="admin.php" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                            <!-- <i class="right fas fa-angle-left"></i> -->
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="master-suplier.php" class="nav-link">
+                        <i class="nav-icon fas fa-warehouse"></i>
+                        <p>Master Suplier</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="master-barang.php" class="nav-link">
+                        <i class="nav-icon fas fa-cubes"></i>
+                        <p>Master Barang</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="master-user.php" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>Master User</p>
+                    </a>
+                </li>
+                <li class="nav-item user-panel pb-1">
+                    <a href="master-user.php" class="nav-link">
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>Master Transaksi</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
